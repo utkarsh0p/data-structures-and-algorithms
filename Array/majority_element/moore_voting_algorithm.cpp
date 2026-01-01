1c304787c43ec09c49c780df485cbf3a9245010e
Utkarsh Singh <utkarshdevendrasingh@gamil.com>
2026-01-02 03:52:48 +0530

    moore voting algorithm

---
+++ Array/majority_element/moore_voting_algorithm.cpp
@@ -0,0 +1,12 @@
+//There will be a frequency give which you have to find the number in the list appearing more than the frequency
+/*
+    approach 1 = Brute force
+    approach 2 = Sort and count the frequecy if equal give back
+    approach 3 = Moore's voting algorithm
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+}
